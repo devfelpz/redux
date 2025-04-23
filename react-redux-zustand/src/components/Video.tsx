876935c2972c4bfce2423fcfa1ae44e4327ac4b1
Felipe <fela.19silva@gmail.com>
2025-04-23 15:35:24 -0300

    refactor: removida chamada repetida para buscar a aula atual e criado hook global

--- react-redux-zustand/src/components/Video.tsx
+++ react-redux-zustand/src/components/Video.tsx
@@ -1,29 +1,22 @@
 import ReactPlayer from "react-player";
 import { useDispatch } from "react-redux";
-import { useAppSelector } from "../store";
-import { next } from "../store/slices/player";
+import { next, useCurrentLesson } from "../store/slices/player";
 
 const Video = () => {
     const dispatch = useDispatch();
-
+    const { currentLesson } = useCurrentLesson();
 
     function handlePlayNext() {
         dispatch(next())
     }
-    const video = useAppSelector(state => {
-        const { currentModuleIndex, currentLessonIndex } = state.player
 
-        const currentLesson = state.player.course.modules[currentModuleIndex].lessons[currentLessonIndex]
-
-        return currentLesson
-    })
     return (
         <div className="w-full bg-zinc-950 aspect-video">
             <ReactPlayer
                 width="100%"
                 height="100%"
                 controls
-                url={`https://www.youtube.com/watch?v=${video.id}`}
+                url={`https://www.youtube.com/watch?v=${currentLesson.id}`}
                 onEnded={handlePlayNext}
                 playing />
         </div>
